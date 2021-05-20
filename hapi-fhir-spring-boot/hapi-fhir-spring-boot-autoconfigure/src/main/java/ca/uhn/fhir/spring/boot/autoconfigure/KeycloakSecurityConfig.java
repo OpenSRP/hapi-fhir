6d5577f455e9e1811af098b8ad40287b7833e11a
Reham Muzzamil <reham.muzzamil@venturedive.com>
2021-05-20 12:22:52 +0500

    40 : Added keycloak configuration

--- hapi-fhir-spring-boot/hapi-fhir-spring-boot-autoconfigure/src/main/java/ca/uhn/fhir/spring/boot/autoconfigure/KeycloakSecurityConfig.java
+++ hapi-fhir-spring-boot/hapi-fhir-spring-boot-autoconfigure/src/main/java/ca/uhn/fhir/spring/boot/autoconfigure/KeycloakSecurityConfig.java
@@ -8,14 +8,17 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.AdapterDeploymentContextFactoryBean;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakSpringConfigResolverWrapper;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
@@ -27,38 +30,92 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @KeycloakConfiguration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
 	@Value("${keycloak.configurationFile:keycloak.json}")
 	private Resource keycloakConfigFileResource;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests()
-            .anyRequest()
-            .permitAll();
-        http.csrf().disable();
-    }
+	@Autowired
+	private KeycloakClientRequestFactory keycloakClientRequestFactory;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
+	private static final String CORS_ALLOWED_HEADERS = "origin,content-type,accept,x-requested-with,Authorization";
 
-    @Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
+	/**
+	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+		auth.authenticationProvider(keycloakAuthenticationProvider);
+	}
 
-    @Bean
-    public KeycloakConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
+	/**
+	 * Defines the session authentication strategy.
+	 */
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		super.configure(http);
+		/* @formatter:off */
+//		http
+//			.csrf().disable() // <- THIS LINE
+//			.cors().disable()
+//			.authorizeRequests()
+////			.antMatchers("/**", "/fhir/**", "/fhir/")
+////			.hasAnyRole()
+//			.anyRequest()
+//			.authenticated();
+
+		//working
+		http.authorizeRequests()
+			.anyRequest()
+			.permitAll();
+		http.csrf().disable();
+		//working end
+
+//		http
+//			.authorizeRequests()
+//			.anyRequest().authenticated()
+//			.and()
+//			.csrf()
+//			.ignoringAntMatchers("/fhir/**","/fhir/patient/**")
+//			.and()
+//			.logout()
+//			.logoutRequestMatcher(new AntPathRequestMatcher("logout.do", "GET"));
+
+		/* @formatter:on */
+
+	}
+
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(Arrays.asList(opensrpAllowedSources.split(",")));
+//		configuration.setAllowedMethods(Arrays.asList(GET.name(), POST.name(), PUT.name(), DELETE.name()));
+//		configuration.setAllowedHeaders(Arrays.asList(CORS_ALLOWED_HEADERS.split(",")));
+//		configuration.setMaxAge(corsMaxAge);
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
+
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	@Autowired
+	public KeycloakRestTemplate keycloakRestTemplate() {
+		return new KeycloakRestTemplate(keycloakClientRequestFactory);
+	}
+
+	@Bean
+	public KeycloakConfigResolver KeycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
 
 	@Bean
 	public KeycloakDeployment keycloakDeployment() throws IOException {
@@ -67,13 +124,14 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
 				keycloakConfigFileResource.getFilename()));
 		}
 
-		try(InputStream inputStream=keycloakConfigFileResource.getInputStream()){
+		try (InputStream inputStream = keycloakConfigFileResource.getInputStream()) {
 			return KeycloakDeploymentBuilder.build(inputStream);
 		}
 
 	}
 
 	@Bean
+	@Override
 	protected AdapterDeploymentContext adapterDeploymentContext() throws Exception {
 		AdapterDeploymentContextFactoryBean factoryBean;
 		if (this.KeycloakConfigResolver() != null) {
